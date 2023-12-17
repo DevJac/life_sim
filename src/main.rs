5e5672fd46999b38b56c0fb2db3dea8ceee4dea1
DevJac <dbuttonz@gmail.com>
2023-12-17 13:01:45 -0700

    Create window, can close window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use winit::{
+    event_loop::{ControlFlow, EventLoop},
+    window::Window,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new().unwrap();
+    let _window = Window::new(&event_loop);
+    event_loop.set_control_flow(ControlFlow::Poll);
+    event_loop
+        .run(move |event, event_loop_window_target| match event {
+            winit::event::Event::WindowEvent {
+                window_id: _,
+                event: window_event,
+            } => match window_event {
+                winit::event::WindowEvent::CloseRequested
+                | winit::event::WindowEvent::KeyboardInput {
+                    device_id: _,
+                    event:
+                        winit::event::KeyEvent {
+                            physical_key: _,
+                            logical_key:
+                                winit::keyboard::Key::Named(winit::keyboard::NamedKey::Escape),
+                            text: _,
+                            location: _,
+                            state: _,
+                            repeat: _,
+                            ..
+                        },
+                    is_synthetic: _,
+                } => {
+                    event_loop_window_target.exit();
+                }
+                _ => {}
+            },
+            _ => {}
+        })
+        .unwrap();
 }
