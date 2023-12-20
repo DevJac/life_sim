6a9e7f87a012d6c8b4aa4c6bcdfa6e725046c6bb
DevJac <dbuttonz@gmail.com>
2023-12-19 22:20:24 -0700

    Line drawing WIP

--- src/life_sim.rs
+++ src/life_sim.rs
@@ -1,8 +1,25 @@
 use pollster::FutureExt as _;
 
-/// We will use normalized device coordinates (NDC) as our world coordinates,
-/// because NDC is as good a world coordinate system as any other.
-/// This means coordinates between -1 and 1 on both the x and y axis.
+trait VertexAttributes {
+    fn vertex_attributes() -> &'static [wgpu::VertexAttribute];
+}
+
+#[repr(C)]
+#[derive(Clone, Copy, bytemuck::Zeroable, bytemuck::Pod)]
+struct LineVertex(glam::Vec2);
+
+const LINE_VERTEX_ATTRIBUTES: &[wgpu::VertexAttribute] = &[wgpu::VertexAttribute {
+    format: wgpu::VertexFormat::Float32x2, // glam::Vec2 size = 4 * 2 = 8
+    offset: 0,
+    shader_location: 0,
+}];
+
+impl VertexAttributes for LineVertex {
+    fn vertex_attributes() -> &'static [wgpu::VertexAttribute] {
+        LINE_VERTEX_ATTRIBUTES
+    }
+}
+
 #[repr(C)]
 #[derive(Clone, Copy, bytemuck::Zeroable, bytemuck::Pod)]
 pub struct Line {
@@ -12,17 +29,119 @@ pub struct Line {
     color: glam::Vec3,
 }
 
+const LINE_ATTRIBUTES: &[wgpu::VertexAttribute] = &[
+    wgpu::VertexAttribute {
+        format: wgpu::VertexFormat::Float32x2, // a size = 4 * 2 = 8
+        offset: 0,
+        shader_location: 0,
+    },
+    wgpu::VertexAttribute {
+        format: wgpu::VertexFormat::Float32x2, // b size = 4 * 2 = 8
+        offset: 8,
+        shader_location: 1,
+    },
+    wgpu::VertexAttribute {
+        format: wgpu::VertexFormat::Float32x3, // b size = 4 * 3 = 12
+        offset: 16,
+        shader_location: 2,
+    },
+];
+
+impl VertexAttributes for Line {
+    fn vertex_attributes() -> &'static [wgpu::VertexAttribute] {
+        LINE_ATTRIBUTES
+    }
+}
 /// Encodes commands to draw the given lines to the given texture. Returns a CommandBuffer.
 pub fn draw_lines(
     device: &wgpu::Device,
+    preferred_texture_format: wgpu::TextureFormat,
     lines: &[Line],
     texture_view: &wgpu::TextureView,
 ) -> wgpu::CommandBuffer {
+    let shader_module = device.create_shader_module(wgpu::include_wgsl!("shaders/line.wgsl"));
+    let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+        label: Some("lines render pipeline"),
+        layout: None,
+        vertex: wgpu::VertexState {
+            module: &shader_module,
+            entry_point: "vertex_main",
+            buffers: &[
+                wgpu::VertexBufferLayout {
+                    array_stride: std::mem::size_of::<LineVertex>() as u64,
+                    step_mode: wgpu::VertexStepMode::Vertex,
+                    attributes: LineVertex::vertex_attributes(),
+                },
+                wgpu::VertexBufferLayout {
+                    array_stride: std::mem::size_of::<Line>() as u64,
+                    step_mode: wgpu::VertexStepMode::Instance,
+                    attributes: Line::vertex_attributes(),
+                },
+            ],
+        },
+        primitive: wgpu::PrimitiveState {
+            topology: wgpu::PrimitiveTopology::LineList,
+            strip_index_format: None,
+            front_face: wgpu::FrontFace::Ccw,
+            cull_mode: None,
+            unclipped_depth: false,
+            polygon_mode: wgpu::PolygonMode::Fill,
+            conservative: false,
+        },
+        depth_stencil: None,
+        multisample: wgpu::MultisampleState::default(),
+        fragment: Some(wgpu::FragmentState {
+            module: &shader_module,
+            entry_point: "fragment_main",
+            targets: &[Some(wgpu::ColorTargetState {
+                format: preferred_texture_format,
+                blend: None,
+                write_mask: wgpu::ColorWrites::ALL,
+            })],
+        }),
+        multiview: None,
+    });
+    let line_vertices = [
+        LineVertex(glam::Vec2::new(0.0, 0.0)),
+        LineVertex(glam::Vec2::new(1.0, 1.0)),
+    ];
+    let line_vertices_bytes = bytemuck::bytes_of(&line_vertices);
+    let line_vertex_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+        label: Some("line vertex buffer"),
+        size: line_vertices_bytes.len() as u64,
+        usage: wgpu::BufferUsages::VERTEX,
+        mapped_at_creation: true,
+    });
+    line_vertex_buffer
+        .slice(..)
+        .get_mapped_range_mut()
+        .copy_from_slice(line_vertices_bytes);
+    line_vertex_buffer.unmap();
+    let lines_bytes: &[u8] = bytemuck::cast_slice(lines);
+    let line_instance_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+        label: Some("line instance buffer"),
+        size: lines_bytes.len() as u64,
+        usage: wgpu::BufferUsages::VERTEX,
+        mapped_at_creation: true,
+    });
+    line_instance_buffer
+        .slice(..)
+        .get_mapped_range_mut()
+        .copy_from_slice(lines_bytes);
+    line_instance_buffer.unmap();
+    let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+        label: Some("line bind group"),
+        layout: &render_pipeline.get_bind_group_layout(0),
+        entries: &[wgpu::BindGroupEntry {
+            binding: todo!(),
+            resource: todo!(),
+        }],
+    });
     let mut command_encoder =
         device.create_command_encoder(&wgpu::CommandEncoderDescriptor::default());
     {
-        let _render_pass = command_encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-            label: Some("rennder pass"),
+        let mut render_pass = command_encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            label: Some("lines render pass"),
             color_attachments: &[Some(wgpu::RenderPassColorAttachment {
                 view: &texture_view,
                 resolve_target: None,
@@ -40,6 +159,7 @@ pub fn draw_lines(
             timestamp_writes: None,
             occlusion_query_set: None,
         });
+        render_pass.set_pipeline(&render_pipeline);
     }
     command_encoder.finish()
 }
@@ -101,7 +221,12 @@ impl LifeSim {
         let surface_texture_view: wgpu::TextureView = surface_texture
             .texture
             .create_view(&wgpu::TextureViewDescriptor::default());
-        let draw_lines_command_buffer = draw_lines(&self.device, &[], &surface_texture_view);
+        let draw_lines_command_buffer = draw_lines(
+            &self.device,
+            self.preferred_texture_format,
+            &[],
+            &surface_texture_view,
+        );
         self.queue.submit([draw_lines_command_buffer]);
         surface_texture.present();
     }
---
+++ src/shaders/line.wgsl
@@ -0,0 +1,62 @@
+struct LineVertex {
+    @location(0) position: vec2f,
+}
+
+struct LineInstance {
+    @location(0) a: vec2f,
+    @location(1) b: vec2f,
+    @location(2) color: vec3f,
+}
+
+
+
+
+// The size of the full low res canvas; e.g., 800 x 600.
+// struct Camera {
+//     @location(0) top_left: vec2f,
+//     @location(1) width_height: vec2f,
+// };
+// 
+// struct TextureVertex {
+//     @location(0) position: vec3f,
+//     @location(1) uv: vec2f,
+//     @location(2) lower_right: vec3u,
+// };
+// 
+// struct TextureFragment {
+//     @builtin(position) position: vec4f,
+//     @location(1) uv: vec2f,
+//     @location(2) @interpolate(flat) lower_right: vec3u,
+// };
+// 
+// @group(0) @binding(0) var<uniform> camera: Camera;
+// @group(0) @binding(1) var textures_sampler: sampler;
+// @group(0) @binding(2) var textures: texture_2d_array<f32>;
+// 
+// @vertex
+// fn vertex_main(vertex: TextureVertex) -> TextureFragment {
+//     // Adjust coordinates in our world space (e.g., somewhere in the 800 x 600 grid)
+//     // to normalized device coordinates (NDC, e.g., somewhere in the -1 to 1 range).
+//     let ndc = vec4f(
+//         (vertex.position.x - camera.top_left.x) / f32(camera.width_height.x) * 2.0 - 1.0,
+//         (vertex.position.y - camera.top_left.y) / f32(camera.width_height.y) * 2.0 - 1.0,
+//         vertex.position.z,
+//         1.0,
+//     );
+//     return TextureFragment(ndc, vertex.uv, vertex.lower_right);
+// }
+// 
+// @fragment
+// fn fragment_main(fragment: TextureFragment) -> @location(0) vec4f {
+//     let full_dims: vec2u = textureDimensions(textures);
+//     let adjusted_uv = vec2f(
+//         fragment.uv.x * (f32(fragment.lower_right.x) / f32(full_dims.x)),
+//         fragment.uv.y * (f32(fragment.lower_right.y) / f32(full_dims.y)),
+//     );
+//     return textureSample(textures, textures_sampler, adjusted_uv, fragment.lower_right.z);
+// }
+// 
+// @fragment
+// fn fragment_line(fragment: TextureFragment) -> @location(0) vec4f {
+//     return vec4f(1.0, 1.0, 0.0, 1.0);
+// }
