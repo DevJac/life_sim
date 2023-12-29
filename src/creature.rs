4da715f16e1ebca02571def9b0372efed60af556
DevJac <dbuttonz@gmail.com>
2023-12-29 14:54:12 -0700

    Creature.segment_lengths(), Creature.energy_income(), Creature.energy_requirement()

--- src/creature.rs
+++ src/creature.rs
@@ -7,10 +7,32 @@ pub enum SegmentType {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
+pub struct SegmentLengths {
+    pub energy: f32,
+    pub attack: f32,
+    pub defend: f32,
+    pub move_: f32,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Segment {
     pub a: glam::Vec2,
     pub b: glam::Vec2,
     pub t: SegmentType,
+}
+
+impl Segment {
+    pub fn length(&self) -> f32 {
+        let x_diff = self.a.x - self.b.x;
+        let y_diff = self.a.y - self.b.y;
+        (x_diff.powi(2) + y_diff.powi(2)).sqrt()
+    }
+
+    fn max_dist_from_origin_squared(&self) -> f32 {
+        let a_dist = self.a.x.powi(2) + self.a.y.powi(2);
+        let b_dist = self.b.x.powi(2) + self.b.y.powi(2);
+        a_dist.max(b_dist)
+    }
 }
 
 pub struct Creature {
@@ -40,23 +62,42 @@ impl Default for Creature {
     }
 }
 
-fn dist_from_origin_squared(v: glam::Vec2) -> f32 {
-    v.x.powi(2) + v.y.powi(2)
-}
-
 impl Creature {
     pub fn radius(&self) -> f32 {
         let mut radius_squared = 0.0;
         for segment in self.segments.iter() {
-            let dfos = dist_from_origin_squared(segment.a);
-            if dfos > radius_squared {
-                radius_squared = dfos;
-            }
-            let dfos = dist_from_origin_squared(segment.b);
+            let dfos = segment.max_dist_from_origin_squared();
             if dfos > radius_squared {
                 radius_squared = dfos;
             }
         }
         radius_squared.sqrt()
+    }
+
+    pub fn segment_lengths(&self) -> SegmentLengths {
+        let mut segment_lengths = SegmentLengths {
+            energy: 0.0,
+            attack: 0.0,
+            defend: 0.0,
+            move_: 0.0,
+        };
+        for segment in self.segments.iter() {
+            match segment.t {
+                SegmentType::Energy => segment_lengths.energy += segment.length(),
+                SegmentType::Attack => segment_lengths.attack += segment.length(),
+                SegmentType::Defend => segment_lengths.defend += segment.length(),
+                SegmentType::Move => segment_lengths.move_ += segment.length(),
+            }
+        }
+        segment_lengths
+    }
+
+    pub fn energy_income(&self) -> f32 {
+        self.segment_lengths().energy
+    }
+
+    pub fn energy_requirement(&self) -> f32 {
+        let segment_lengths = self.segment_lengths();
+        segment_lengths.attack + segment_lengths.defend + segment_lengths.move_ + self.radius()
     }
 }
